12d2b4eacac60d732fd8346cc2a1135de8eeb734
Decky Coss <deckycoss@gmail.com>
2019-10-21 22:22:01 -0600

    add cell state toggling

--- src/eca/core.cljs
+++ src/eca/core.cljs
@@ -2,6 +2,13 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [bitset]))
 
+(defonce bitsets (-> #(. bitset Random)
+                  (map (range 16))
+                  vec
+                  atom))
+
+(defonce cell-size (atom 20))
+
 (defn cell [{:keys [size row-index col-index state]}]
   (let [attrs {:width size
                :height size
@@ -10,36 +17,52 @@
                :fill (if-not state "none")}]
     [:rect attrs]))
 
-(defn row [{:keys [data cell-size row-index]}]
-  (println (. data toString))
+(defn row [{:keys [col-indices cell-size row-index]}]
   (into [:g]
           (map-indexed
            #(vector cell {:size cell-size
                           :row-index row-index
                           :col-index %2
                           :state true})
-           (js->clj (. data toArray)))))
+           col-indices)))
 
-(defn grid [{:keys [rows cell-size]}]
+(defn grid [{:keys [col-index-vecs cell-size]}]
   (into [:g]
           (map-indexed
-           #(vector row {:cell-size cell-size :row-index %1 :data %2})
-           rows)))
+           #(vector row {:cell-size cell-size :row-index %1 :col-indices %2})
+           col-index-vecs)))
+
+(defn flip-bit [coll, row-index, col-index]
+  (update coll
+          row-index
+          (fn [bs]
+            (let [newbs (. bs (flip col-index))]
+              newbs))))
+
+(defn on-svg-click [e]
+  (let [bound (.. e -target getBoundingClientRect)
+        s @cell-size
+        x (- (. e -clientX) (. bound -left))
+        y (- (. e -clientY) (. bound -top))
+        col-index (. js/Math floor (/ x s))
+        row-index (. js/Math floor (/ y s))]
+    (swap! bitsets flip-bit row-index col-index 0)))
+    ;(println (. js/Math floor (/ x s)))))
 
 (defn app []
-  (let [rows (-> #(. bitset Random)
-                 (map (range 16))
-                 vec
-                 atom)]
-    (fn []
-      [:svg {:xmlns "http://www.w3.org/2000/svg"
-             :fill "black"
-             :width "100%"
-             :height "100%"
-             :stroke "none"
-             :shape-rendering "crispEdges"
-             :pointer-events "all"}
-       [grid {:rows @rows :cell-size 20}]])))
+  [:svg {:xmlns "http://www.w3.org/2000/svg"
+         :fill "black"
+         :width "100%"
+         :height "100%"
+         :stroke "none"
+         :pointerEvents "all"
+         :shape-rendering "crispEdges"}
+   [grid {:col-index-vecs (map #(js->clj (. % toArray)) @bitsets)
+          :cell-size @cell-size}]
+   [:rect {:fill "none"
+           :onClick on-svg-click
+           :width (* 20 32)
+           :height (* 20 16)}]])
 
 (defn start []
   (reagent/render-component [app]
